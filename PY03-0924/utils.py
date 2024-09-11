ee1ad8c7e32b425843e5fb570d83d84fd70e15f2
José Areia <jose.apareia@gmail.com>
2024-09-11 16:34:29 +0100

    add: dcgan with on-demand generation

---
+++ PY03-0924/utils.py
@@ -0,0 +1,23 @@
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+
+# Directory containing the data.
+root = '../Datasets/TRM-UAP'
+
+def get_dataset(params):
+    # Data proprecessing.
+    transform = transforms.Compose([
+        transforms.Resize(params['imsize']),
+        transforms.CenterCrop(params['imsize']),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # Create the dataset.
+    dataset = dset.ImageFolder(root=root, transform=transform)
+
+    # Create the dataloader.
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=params['bsize'], shuffle=True)
+    
+    return dataloader
