2da43132df6e1616cf31243d8c71969827275ba1
José Areia <jose.apareia@gmail.com>
2024-10-31 17:34:14 +0000

    add: superstargan source code modified

---
+++ SuperstarGAN/run-test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python main.py --mode test --dataset ImageNet --attr_path ../Datasets/ImageNet5/image_data.txt --c_dim 2 --log_dir imagenet5/logs --model_save_dir imagenet5/models --sample_dir imagenet5/samples --result_dir imagenet5/results
