c2ef9d8aeb0cfac84191f9b394fc3307600c695e
José Areia <jose.apareia@gmail.com>
2024-11-06 14:40:42 +0000

    fix: passed arguments

--- SuperstarGAN/run-test.sh
+++ SuperstarGAN/run-test.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-python src/main.py --mode test \
-    --dataset ImageNet \
-    --attr_path ../Datasets/ImageNet5/image_data.txt \
-    --c_dim 2 \
-    --log_dir imagenet5/logs \
-    --model_save_dir imagenet5/models \
-    --sample_dir imagenet5/samples \
-    --result_dir imagenet5/results
+python src/main.py --mode test --dataset ImageNet --c_dim 2
