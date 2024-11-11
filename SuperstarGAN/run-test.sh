be06f94d8772f0e39b31360e896c3791251e243d
José Areia <jose.apareia@gmail.com>
2024-11-11 16:43:15 +0000

    patch: dataset directory

--- SuperstarGAN/run-test.sh
+++ SuperstarGAN/run-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python src/main.py --mode test --dataset ImageNet --c_dim 2
+python src/main.py --mode test --dataset ImageNet --imagenet_image_dir ../../Datasets/ImageNet5/Images-Test --c_dim 2
