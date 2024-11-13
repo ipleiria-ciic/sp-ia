225b59cbcbc92f7d8700a82da6e76e1d5654e8c7
José Areia <jose.apareia@gmail.com>
2024-11-13 15:46:38 +0000

    add: auto log maker

--- SuperstarGAN/src/classification.py
+++ SuperstarGAN/src/classification.py
@@ -3,14 +3,16 @@
     images and export the accuracy results for each trial.
 """
 
+import os
 import time
 import torch
 import utils
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 img_original_path = "../../../Datasets/ImageNet5/Images"
 img_adversarial_path = "../results-transformed/imagenet"
 img_to_class = "../../../Datasets/ImageNet5/image_to_class.txt"
+log_path = "../logs/classification"
 
 def classifier(model, device, img_given):
     was_training = model.training
@@ -80,5 +82,17 @@ print(f"[ RESULTS ] A total of {acc_adversarial} adversarial images successfully
 
 # End time.
 elapsed = time.time() - start_time
+
+# Create log file with the following information: datetime, total_images, acc_original and acc_adversarial.
+os.makedirs(log_path, exist_ok=True)
+txt_class_path = os.path.join(log_path, "log.txt")
+with open(txt_class_path, mode="a") as file:
+    file.write(f"Classification created on: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.\n")
+    file.write(f"Total images classified: {total_images}.\n")
+    file.write(f"Original accuracy classification: {((acc_original*100)/total_images):.2f}%\n")
+    file.write(f"Adversarial accuracy classification (in a total of {acc_original} images): {((acc_adversarial*100)/acc_original):.2f}%\n")
+    file.write(f"Total adversarial images that fooled the classifier: {acc_adversarial}.\n")
+    file.write(f"Classification completed in '{str(timedelta(seconds=int(elapsed)))}'\n")
+    file.write(f"---\n")
 
 print(f"[ INFO ] Classification completed in '{str(timedelta(seconds=int(elapsed)))}'.")
