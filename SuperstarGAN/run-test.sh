11527f3880d6de9200b4058094ef66ead340d2a8
José Areia <jose.apareia@gmail.com>
2025-01-30 15:58:47 +0000

    refactor: add new line between arguments

--- SuperstarGAN/run-test.sh
+++ SuperstarGAN/run-test.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-python src/main.py --mode test --dataset ImageNet --test_iters 500000 --imagenet_image_dir ../../Datasets/ImageNet5/Images-Test --c_dim 2
+python src/main.py \
+       --mode test \
+       --dataset ImageNet \
+       --test_iters 500000 \
+       --imagenet_image_dir ../../Datasets/ImageNet5/Images-Test \
+       --c_dim 2
