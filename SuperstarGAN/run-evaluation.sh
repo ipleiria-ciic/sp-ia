30249061914f841c92d4ffefee8247694ab78677
joseareia <jose.apareia@gmail.com>
2025-02-03 14:59:31 +0000

    fix: remove unnecessary files

--- SuperstarGAN/run-evaluation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python src/evaluation.py
--- SuperstarGAN/run-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python src/main.py \
-       --mode test \
-       --dataset ImageNet \
-       --test_iters 500000 \
-       --imagenet_image_dir ../../Datasets/ImageNet5/Images-Test \
-       --c_dim 2
